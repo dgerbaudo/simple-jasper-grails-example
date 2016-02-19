f68169abb86fa388ac82008cd367acdf7565a7ff
Daniel Gerbaudo <info@danielgerbaudo.com>
2016-02-19 12:27:11 -0300

    Example

---
+++ grails-app/controllers/test/TestController.groovy
@@ -0,0 +1,19 @@
+package test
+
+import net.sf.jasperreports.engine.JREmptyDataSource
+import org.codehaus.groovy.grails.plugins.jasper.JasperExportFormat
+import org.codehaus.groovy.grails.plugins.jasper.JasperReportDef
+
+class TestController {
+    def jasperService
+    def index() {
+        def reportDef = new JasperReportDef(name:'report.jrxml',
+                fileFormat:JasperExportFormat.PDF_FORMAT, dataSource: new JREmptyDataSource(), parameters: [testParam: 'Hello world']
+        )
+        response.reset();
+//      response.setContentType("application/octet-stream"); // Force browse to download file
+        response.setHeader("Content-disposition", "inline; filename='example.pdf'");
+        response.outputStream << jasperService.generateReport(reportDef).toByteArray()
+        response.outputStream.flush()
+    }
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+This is a simple example to show how to use a Jasper report without a DataSource with Grails.
+
+How to try it:
+1. Execute the project
+2. Enter to http://localhost:8080/test/test/index
